61d37525d429a144465a78319f384ce434f0c6cc
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-11-11 16:22:50 +0200

    Add ProxysqlHelpers template helpers

---
+++ libraries/helpers.rb
@@ -0,0 +1,34 @@
+module ProxysqlHelpers
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def to_cnf(obj, comma: false)
+    output = StringIO.new
+    case obj
+    when Hash
+      output.puts '{'
+      obj.each do |k, v|
+        output.puts "  #{k}=#{to_cnf(v)}"
+      end
+      output.puts "}#{comma ? ',' : ''}"
+    when Array
+      last = obj.last
+
+      output.puts '('
+      obj.each do |val|
+        output.puts to_cnf(val, comma: last != val)
+      end
+      output.puts ')'
+    when String
+      output.puts "\"#{obj}\""
+    when Integer || Float
+      output.puts obj
+    when TrueClass || FalseClass
+      output.puts obj.to_s
+    else
+      Chef::Log.warn "proxsql: Object #{obj} of class #{obj.class} "\
+        'is not supported interpreting as a String'
+      output.puts "\"#{obj}\""
+    end
+    output.string
+  end
+end
