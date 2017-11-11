2c52930a75035c0546578c727302b138bab87246
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-11-11 16:16:02 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,10 @@
-name 'chef-proxysql'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures chef-proxysql'
-long_description 'Installs/Configures chef-proxysql'
-version '0.1.0'
+name 'proxysql'
+maintainer 'Ernestas Poskus'
+maintainer_email 'ernestas.poskus@gmail.com'
+license 'BSD 3-Clause License'
+description 'Installs/Configures ProxySQL'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url 'https://github.com/ernestas-poskus/chef-proxysql/issues'
+source_url 'https://github.com/ernestas-poskus/chef-proxysql'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef-proxysql/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef-proxysql'
+version '0.1.0'
