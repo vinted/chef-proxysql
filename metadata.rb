e0ac5cd83601939afc049f88a4491464a6be80e0
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-11-12 19:44:22 +0200

    Change LICENSE to MIT

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name 'proxysql'
 maintainer 'Ernestas Poskus'
 maintainer_email 'ernestas.poskus@gmail.com'
-license 'BSD 3-Clause License'
+license 'MIT'
 description 'Installs/Configures ProxySQL'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/ernestas-poskus/chef-proxysql/issues'
 source_url 'https://github.com/ernestas-poskus/chef-proxysql'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-version '0.1.0'
+version '0.2.0'
 
 supports 'centos'
 
