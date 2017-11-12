99715e22d74b7810b41241a8396b7844d7682687
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-11-12 19:46:34 +0200

    Add issues_url and source_url attributes to metadata

--- test/integration/cookbooks/test-proxysql/metadata.rb
+++ test/integration/cookbooks/test-proxysql/metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'ernestas.poskus@gmail.com'
 license 'MIT'
 description 'Tests proxysql'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/ernestas-poskus/chef-proxysql/issues'
+source_url 'https://github.com/ernestas-poskus/chef-proxysql'
 version '0.1.0'
 
 depends 'proxysql'
