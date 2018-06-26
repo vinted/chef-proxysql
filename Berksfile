83774efe4445de1504589eba4d4c8e1b6c78512d
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-06-26 14:55:37 +0300

    Drop sysvinit support

--- Berksfile
+++ Berksfile
@@ -4,7 +4,7 @@ source 'https://supermarket.chef.io'
 metadata
 
 cookbook 'poise', '~> 2.8.1'
-cookbook 'poise-service', '~> 1.5.2'
+cookbook 'systemd', '~> 3.2.3'
 
 group 'test' do
   cookbook 'test-proxysql', path: 'test/integration/cookbooks/test-proxysql'
--- test/integration/default/proxysql.rb
+++ test/integration/default/proxysql.rb
@@ -16,13 +16,6 @@ describe package('proxysql') do
   it { should be_installed }
 end
 
-# ProxySQL
-describe service('proxysql-first') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
-
 # ProxySQL admin
 describe port(6033) do
   it { should be_listening }
