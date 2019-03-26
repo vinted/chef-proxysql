85eae5869670537c38004d3766d6be51c96f8fcd
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-03-26 12:20:37 +0200

    Use percona latest-release for rhel/debian repositories

--- attributes/repository.rb
+++ attributes/repository.rb
@@ -1,24 +1,13 @@
-default['percona']['repository']['name'] = 'percona-release.repo'
-default['percona']['repository']['version'] = '0.1-4'
+default['percona']['repository']['name'] = 'percona-original-release.repo'
 
 case node['platform']
 when 'rhel', 'centos'
-  default['proxysql']['package_release'] = "1.1.el#{node['platform_version'].to_i}"
-
-  default['percona']['repository']['url'] = 'http://www.percona.com/'\
-    'downloads/percona-release/redhat/'\
-    "#{node['percona']['repository']['version']}/"\
-    "percona-release-#{node['percona']['repository']['version']}.noarch.rpm"
+  default['percona']['repository']['url'] = 'http://repo.percona.com/yum/percona-release-latest.noarch.rpm'
 when 'debian', 'ubuntu'
   lsb_release = Mixlib::ShellOut.new('lsb_release -sc')
   lsb_release.run_command
   lsb_release.error!
   lsb_release = lsb_release.stdout.chomp
 
-  default['proxysql']['package_release'] = "1.1.#{lsb_release}"
-
-  default['percona']['repository']['url'] =
-    'http://repo.percona.com/apt/percona-release_'\
-    "#{node['percona']['repository']['version']}."\
-    "#{lsb_release}_all.deb"
+  default['percona']['repository']['url'] = "http://repo.percona.com/apt/percona-release_latest.#{lsb_release}_all.deb"
 end
