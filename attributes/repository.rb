e434ef34dd1a535925c7de54dcb3a4b6636b4950
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-05-20 16:50:20 +0300

    Add proxysql package release attribute

--- attributes/repository.rb
+++ attributes/repository.rb
@@ -3,11 +3,13 @@ default['proxysql']['repository']['name'] = 'percona-original-release.repo'
 case node['platform']
 when 'rhel', 'centos'
   default['proxysql']['repository']['url'] = 'http://repo.percona.com/yum/percona-release-latest.noarch.rpm'
+  default['proxysql']['package_release'] = "1.1.el#{node['platform_version'].to_i}"
 when 'debian', 'ubuntu'
   lsb_release = Mixlib::ShellOut.new('lsb_release -sc')
   lsb_release.run_command
   lsb_release.error!
   lsb_release = lsb_release.stdout.chomp
 
+  default['proxysql']['package_release'] = "1.1.#{lsb_release}"
   default['proxysql']['repository']['url'] = "http://repo.percona.com/apt/percona-release_latest.#{lsb_release}_all.deb"
 end
